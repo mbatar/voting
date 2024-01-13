44e0420f817ed5aab8e8b7c85fcba45233e992d5
mustafa batar <mustafa.batar@missafir.com>
2024-01-13 21:15:16 +0300

    page.tsx file connected  to apollo/server with apollo/client

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,38 @@
-export default function Home() {
-  return <main>Voting App</main>;
+import { getClient } from "@/lib/client";
+import { gql } from "@apollo/client";
+
+import Image from "next/image";
+
+const query = gql`
+  query Employees {
+    employees {
+      id
+      name
+      point
+      position
+      avatar
+    }
+  }
+`;
+
+export default async function Home() {
+  const { data } = await getClient().query({ query });
+
+  return (
+    <main>
+      {data.employees.map((employee: any, key: any) => (
+        <div key={key}>
+          <Image
+            src={employee.avatar}
+            alt={employee.name}
+            width={500}
+            height={500}
+          />
+          {employee.name}
+          {employee.point}
+          {employee.position}
+        </div>
+      ))}
+    </main>
+  );
 }
