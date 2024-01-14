8c16cb6945b0c895b8543982de986f5131493879
mustafa batar <mustafa.batar@missafir.com>
2024-01-14 16:41:17 +0300

    added slug package and use grapqhl responses in all components

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import { getClient } from "@/lib/client";
-import { GET_ALL_EMPLOYEES } from "@/service/graphql";
 import EmployeeList from "@/components/employeeList/EmployeeList";
 
 export default async function Home() {
-  const { data } = await getClient().query({ query: GET_ALL_EMPLOYEES });
-
   return (
     <main>
-      <EmployeeList employeeList={data.employees} />
+      <EmployeeList />
     </main>
   );
 }
